86924a1b4d1a82bb72edc1eef677cc0bb6c3092e
Murat Ownuk <ilmuratownuk@gmail.com>
2025-01-20 14:15:45 -0500

    finished coding digital_speaker module and the corresponding header file

--- digital_speaker.c
+++ digital_speaker.c
@@ -2,13 +2,47 @@
     digital_speaker: 
     this module contains anything related to the speaker side of the project. 
 */
-#define _DIGITAL_SPEAKER_C_
+#define _DIGITAL_SPEAKER_C_ 
 
-#include "./SiLABS/c8051F000.h"
-#include "digital_speaker.h"
+/*
+    includes 
+*/
+#include "./SiLABS/c8051F000.h" 
+#include "digital_speaker.h" 
 
-sbit SPEAKER = P1^0;
+/*
+    routine prototypes 
+*/
+void vTimer1_ISR(void); 
 
+/*
+    sbit defintions 
+*/
+sbit SPEAKER = P1^0; 
+
+/*
+    global variables 
+*/
+unsigned char count=0; 
+
+/*
+    routines 
+*/
+/*
+    vTimer1_ISR: 
+    services the timer 1 interrupt, which in this case, toggle SPEAKER bit 
+    after 250us*2=500us. 
+*/
+void vTimer1_ISR(void) interrupt TIMER1_INT{
+
+    count++;                            // 250us increment 
+
+    if(count==2){ 
+        count=0;                        // reset 250us increment counter 
+        SPEAKER=~SPEAKER;               // toggle speaker (on/off) 
+    }
+
+}
 
 
 
--- digital_speaker.h
+++ digital_speaker.h
@@ -1,6 +1,20 @@
 #ifndef _DIGITAL_SPEAKER_H_ 
     #define _DIGITAL_SPEAKER_H_ 
 
+    /*
+        defines 
+    */ 
+    #define     TIMER1_INT  3 
+
+    #ifndef _DIGITAL_SPEAKER_C_
+        
+        // function prototypes (for external use) 
+        extern void vTimer1_ISR(void); 
+
+    #endif
+
+
+
 #endif 
 
 
